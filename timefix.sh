b81178731c7a5758a5d2ff9ae3eec758ebf7e26b
lerobinson-beyondtrust <152866457+lerobinson-beyondtrust@users.noreply.github.com>
2023-12-28 10:39:38 +0000

    Update timefix.sh add test for bt.lab

--- timefix.sh
+++ timefix.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-sed -i '3s/.*/server dc01.bt.lab iburst/' /etc/chrony.conf
-sed -i '4,6d' /etc/chrony.conf
-systemctl restart chronyd
+testset=$(grep 'bt.lab' /etc/chrony.conf)
+if [ -z "$testset" ]
+then 
+  sed -i '3s/.*/server dc01.bt.lab iburst/' /etc/chrony.conf
+  sed -i '4,6d' /etc/chrony.conf
+  systemctl restart chronyd
+fi
 echo 'run `chronyc sources` to check the timeserver is set to dc01.bt.lab'
