330cb866cb698b5b7a678979f6458783d082070b
lerobinson-beyondtrust <152866457+lerobinson-beyondtrust@users.noreply.github.com>
2023-12-28 10:29:04 +0000

    Update timefix.sh fix path

--- timefix.sh
+++ timefix.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sed -i '3s/.*/server dc01.bt.lab iburst/' chrony.conf
+sed -i '3s/.*/server dc01.bt.lab iburst/' /etc/chrony.conf
 sed -i '4,6d' /etc/chrony.conf
 systemctl restart chronyd
 chronyc sources
