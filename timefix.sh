617c32bd21ccf1253b0f264870c7600c2cb2a389
lerobinson-beyondtrust <152866457+lerobinson-beyondtrust@users.noreply.github.com>
2024-02-20 08:32:25 +0000

    Update timefix.sh

--- timefix.sh
+++ timefix.sh
@@ -4,6 +4,8 @@ if [ -z "$testset" ]
 then 
   sed -i '3s/.*/server dc01.bt.lab iburst/' /etc/chrony.conf
   sed -i '4,6d' /etc/chrony.conf
+  echo 'maxdistance 100' >> /etc/chrony.conf
   systemctl restart chronyd
+  chronyc makestep
 fi
 echo 'run `chronyc sources` to check the timeserver is set to dc01.bt.lab'
